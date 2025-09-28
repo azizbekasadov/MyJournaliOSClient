a5796a3c153bcbc13946d7d5b6367b010d4d21ec
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2025-09-28 11:59:38 +0200

    minor tweaks for the Post Object

--- MyJournaliOSClient/Models/Post.h
+++ MyJournaliOSClient/Models/Post.h
@@ -11,9 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface Post : NSObject
 
-@property (nonatomic, assign, readonly) int postId;
-@property (nonatomic, copy, readonly) NSString* title;
-@property (nonatomic, copy, readonly) NSString* body;
+@property (nonatomic, assign, readonly, nullable) NSInteger* postId;
+@property (nonatomic, copy, readonly, nullable) NSString* title;
+@property (nonatomic, copy, readonly, nullable) NSString* body;
 
 -(instancetype)initWithDictionary:(NSDictionary *) dict;
 
